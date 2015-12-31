aaf9ff0b51a531ffa3acf086cc0d5f900ebe3368
王天硕 <nelsonnick@126.com>
2015-12-31 15:51:09 +0800

    默认的

---
+++ retire.js
@@ -0,0 +1,110 @@
+/**
+ * Created by 王天硕 on 2015/12/31.
+ */
+    function getCalculateMonth(retireAge){
+        if (retireAge==40){
+            return 233;}
+        else if (retireAge==41){
+            return 230;}
+        else if (retireAge==42){
+            return 226;}
+        else if (retireAge==43){
+            return 223;}
+        else if (retireAge==44){
+            return 220;}
+        else if (retireAge==45){
+            return 216;}
+        else if (retireAge==46){
+            return 212;}
+        else if (retireAge==47){
+            return 208;}
+        else if (retireAge==48){
+            return 204;}
+        else if (retireAge==49){
+            return 199;}
+        else if (retireAge==50){
+            return 195;}
+        else if (retireAge==51){
+            return 190;}
+        else if (retireAge==52){
+            return 185;}
+        else if (retireAge==53){
+            return 180;}
+        else if (retireAge==54){
+            return 175;}
+        else if (retireAge==55){
+            return 170;}
+        else if (retireAge==56){
+            return 164;}
+        else if (retireAge==57){
+            return 158;}
+        else if (retireAge==58){
+            return 152;}
+        else if (retireAge==59){
+            return 145;}
+        else if (retireAge==60){
+            return 139;}
+        else if (retireAge==61){
+            return 132;}
+        else if (retireAge==62){
+            return 125;}
+        else if (retireAge==63){
+            return 117;}
+        else if (retireAge==64){
+            return 109;}
+        else if (retireAge==65){
+            return 101;}
+        else if (retireAge==66){
+            return 93;}
+        else if (retireAge==67){
+            return 84;}
+        else if (retireAge==68){
+            return 75;}
+        else if (retireAge==69){
+            return 65;}
+        else if (retireAge==70){
+            return 56;}
+        else
+            return "超过设定范围，无法识别";
+    }
+    function calculate() {
+
+        var retireAge = document.getElementById("retireAge").value;
+        var latelyWages = document.getElementById("latelyWages").value;
+        var payMonth = document.getElementById("payMonth").value;
+        var averageNumber = document.getElementById("averageNumber").value;
+        var accountBalance = document.getElementById("accountBalance").value;
+        var workYear = document.getElementById("workYear").value;
+        var workMonth = document.getElementById("workMonth").value;
+        var buildYear = document.getElementById("buildYear").value;
+        var buildMonth = document.getElementById("buildMonth").value;
+
+        var basicMoney = document.getElementById("basicMoney");
+        var limboMoney= document.getElementById("limboMoney");
+        var accountMoney= document.getElementById("accountMoney");
+        var totalMoney= document.getElementById("totalMoney");
+        var CalculateMonth=getCalculateMonth(retireAge);
+
+        var work= new Date(workYear, workMonth, "1", "0", "0", "0");
+        var build= new Date(buildYear, buildMonth, "1", "0", "0", "0");
+        var difference=(build.getYear()-work.getYear())*12+build.getMonth()-work.getMonth();
+
+
+        basicMoney.innerHTML = ":"+((parseInt(latelyWages) + latelyWages * averageNumber) / 2 * payMonth /12* 0.01).toFixed(3)+"元";
+        accountMoney.innerHTML = ":"+(accountBalance/CalculateMonth).toFixed(3)+"元";
+
+        if(build>work){
+            limboMoney.innerHTML = ":"+(latelyWages * averageNumber *0.013*difference/12).toFixed(3)+"元";
+            totalMoney.innerHTML = ":"+(parseFloat(((parseInt(latelyWages) + latelyWages * averageNumber) / 2 * payMonth /12* 0.01).toFixed(3))+parseFloat((latelyWages * averageNumber *0.013*difference/12).toFixed(3))+parseFloat(accountBalance/CalculateMonth).toFixed(3))+"元";}
+
+    else{
+            limboMoney.innerHTML = ":"+0.000+"元";
+            totalMoney.innerHTML = ":"+(parseFloat((parseInt(latelyWages) + latelyWages * averageNumber) / 2 * payMonth /12* 0.01).toFixed(3))+parseFloat((accountBalance/CalculateMonth).toFixed(3))+"元";
+        }
+
+
+
+
+
+
+    }
